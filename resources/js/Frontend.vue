ecd4a828e1b55fa05b887689acdc872ada079e5e
GiacomoDiNardo <vest_95@hotmail.it>
2022-08-30 18:04:15 +0200

    add post list in homepage

--- resources/js/Frontend.vue
+++ resources/js/Frontend.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
-        <h1>Hello World</h1>
+        <Navbar></Navbar>
+        <div class="container">
+
+            <h1>QUESTA È LA HOME PAGE PUBBLICA FATTA CON VUE </h1>
+            <h2>Lista post</h2>
+            <Posts></Posts>
+        </div>
+
     </div>
 </template>
 
 <script>
-    export default {
-
-    }
+import Navbar from './frontend/components/Navbar.vue';
+import Posts from './frontend/components/Posts.vue';
+export default {
+    components: { Navbar, Posts },
+}
 </script>
